9fe3a82665e6012174aa6c84f2048418db6a5d19
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-26 10:16:50 -0500

    added submit event in client and listen it in server

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io(); //we have access to io because of script  tag in chat.html
 
 socket.on('message', (message) => {//reciving socket.emit from server.js
-  console.log(message); //in browser console we got the message('Welcome to ChatCord!')
+  // console.log(message); //in browser console we got the message('Welcome to ChatCord!')
 });
+
+//Message submit:
+chatForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // Get message text                 //msg is id for input
+  const message = event.target.elements.msg.value;
+  // console.log(message); //saw it in browser console
+  socket.emit('chatMessage', message); //Emit message to server
+})
--- server.js
+++ server.js
@@ -12,9 +12,25 @@ app.use(express.static(path.join(__dirname, 'public'))); //after adding this bro
 
 // Run it whenever a client connects:
 io.on('connection', (socket) => {
-  console.log('New WebSocket Connection...');
+  // console.log('New WebSocket Connection...');
 
+  // Welcome connect user(to single client):
   socket.emit('message', 'Welcome to ChatCord!'); //we'll take it on main.js
+
+  // Broadcast when a user connects: (to all clients, except the client that's connecting)
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  //Listen for chat message
+  socket.on('chatMessage', (message) => {
+    // console.log(message);//saw it in terminal console
+    //Emit it back to the client:
+    io.emit('message', message)
+  });
 });
 
 const PORT = 8080 || process.env.PORT;
