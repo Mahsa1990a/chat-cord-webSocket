1e75424e04f6ff7a028bae9baccd2121054ecc30
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-26 11:29:35 -0500

    having an object for message instead of only string

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ const chatMessages = document.querySelector('.chat-messages');
 const socket = io(); //we have access to io because of script  tag in chat.html
 
 socket.on('message', (message) => {//reciving socket.emit from server.js
-  // console.log(message); //in browser console we got the message('Welcome to ChatCord!')
+  console.log(message); //in browser console we got the message('Welcome to ChatCord!')
   outputMessage(message); //let's create a function to output msg to DOM
 
   //Automatically scroll down after adding new msg:
@@ -29,8 +29,8 @@ function outputMessage(message) {
   const div = document.createElement('div'); //we create a new div
   div.classList.add('message'); //add message class that we already have in chat.html
   div.innerHTML = ` 
-    <p class="meta">Brad <span>9:12pm</span></p>
-    <p class="text">${message}</p>
+    <p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">${message.text}</p>
   `; //copy the div from chat html
   document.querySelector('.chat-messages').appendChild(div);//append new div to previous
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path'); //nodejs core module
 const http = require('http'); //http module
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,26 +11,31 @@ const io = socketio(server);
 // set public folder as static folder => we can access html files and frontend
 app.use(express.static(path.join(__dirname, 'public'))); //after adding this browser will show index.html
 
+const botName = 'ChatCord Bot';
 // Run it whenever a client connects:
 io.on('connection', (socket) => {
   // console.log('New WebSocket Connection...');
 
   // Welcome connect user(to single client):
-  socket.emit('message', 'Welcome to ChatCord!'); //we'll take it on main.js
+  // socket.emit('message', 'Welcome to ChatCord!'); //we'll take it on main.js  //UPDATE after adding formatMessage
+  socket.emit('message', formatMessage(botName,'Welcome to ChatCord!'));
 
   // Broadcast when a user connects: (to all clients, except the client that's connecting)
-  socket.broadcast.emit('message', 'A user has joined the chat');
+  // socket.broadcast.emit('message', 'A user has joined the chat');//UPDATE after adding formatMessage
+  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    // io.emit('message', 'A user has left the chat');//UPDATE after adding formatMessage
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
   });
 
   //Listen for chat message
   socket.on('chatMessage', (message) => {
     // console.log(message);//saw it in terminal console
     //Emit it back to the client:
-    io.emit('message', message)
+    // io.emit('message', message)//UPDATE after adding formatMessage
+    io.emit('message', formatMessage('USER', message))
   });
 });
 
